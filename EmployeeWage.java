87988b7cb23b0f18939b9a78274090079e32b073
priyankaugile <ugilepriyanka26@gmail.com>
2021-08-26 14:38:48 +0530

    Added: Display Welcome Message

---
+++ EmployeeWage.java
@@ -0,0 +1,9 @@
+public class Start {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee\n" +
+                "Wage Computation\n" +
+                "Program on Master Branch");
+    }
+}
