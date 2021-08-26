47c8ae63b7fe8cc824aa40148188bb467695bd86
priyankaugile <ugilepriyanka26@gmail.com>
2021-08-26 14:45:58 +0530

    Added: code of employee attendance.

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,9 +1,15 @@
-public class Start {
-
+public class Attendance {
+    
     public static void main(String[] args) {
+         //Constants
+        
+        double empCheck = Math.floor(Math.random()* 10)%2;
+         //Computation
+        if(empCheck == 1 )
+        System.out.println("Employee is present");
+        else
+            System.out.println("Employee is not present");
 
-        System.out.println("Welcome to Employee\n" +
-                "Wage Computation\n" +
-                "Program on Master Branch");
     }
 }
+
